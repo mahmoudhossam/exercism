93991eaa746cef10543eb6a5ec0c1d6e98aba527
Mahmoud Hossam <mahmoud.hossam@arabyads.com>
2019-02-05 10:58:57 +0200

    Python word count enhancement

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -3,11 +3,11 @@ import re
 
 
 def word_count(phrase):
-    words = re.split(r"[_, :\.\n\t]", phrase)
+    words = re.split(r"[^a-zA-Z1-9']", phrase)
     for i, word in enumerate(words):
         if word and word[0] == "'" and word[-1] == "'":
             word = word[1:-1]
-        words[i] = re.sub("[, _:.!&@$%^]", "", word.lower())
+        words[i] = word.lower()
     words = filter(None, words)
     c = Counter(words)
-    return dict(c)
+    return c
